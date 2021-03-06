c406a2d391b5adba089d1f1a4dad7474afe1acbf
Kepva <vladyslav.ovsiychuk@gmail.com>
2021-03-06 22:00:16 +0100

    initial commit

---
+++ src/main/java/letscode/interpres/InterpresApplication.java
@@ -0,0 +1,12 @@
+package letscode.interpres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterpresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterpresApplication.class, args);
+	}
+}
