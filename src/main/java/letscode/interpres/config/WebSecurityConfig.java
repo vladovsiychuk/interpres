c6e88be153d5d9f03181f7be52b0a0780f0d206d
Kepva <vladyslav.ovsiychuk@gmail.com>
2021-03-19 19:47:04 +0100

    . (save new authenticated user and clean code)

--- src/main/java/letscode/interpres/config/WebSecurityConfig.java
+++ src/main/java/letscode/interpres/config/WebSecurityConfig.java
@@ -1,17 +1,17 @@
 package letscode.interpres.config;
 
-//import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-//import org.springframework.boot.autoconfigure.security.oauth2.resource.PrincipalExtractor;
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
+import letscode.interpres.services.CustomOidcUserService;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-import java.time.LocalDateTime;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private CustomOidcUserService customOidcUserService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -19,30 +19,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/").permitAll()
                 .anyRequest().authenticated()
-                .and().oauth2Login();
+                .and().oauth2Login().userInfoEndpoint()
+                .oidcUserService(customOidcUserService);
     }
-
-//    @Bean
-//    public PrincipalExtractor principalExtractor(UserDetailsRepo userDetailsRepo) {
-//        return map -> {
-//            String id = (String) map.get("sub");
-//
-//            User user = userDetailsRepo.findById(id).orElseGet(() -> {
-//                User newUser = new User();
-//
-//                newUser.setId(id);
-//                newUser.setName((String) map.get("name"));
-//                newUser.setEmail((String) map.get("email"));
-//                newUser.setGender((String) map.get("gender"));
-//                newUser.setLocale((String) map.get("locale"));
-//                newUser.setUserpic((String) map.get("picture"));
-//
-//                return newUser;
-//            });
-//
-//            user.setLastVisit(LocalDateTime.now());
-//
-//            return userDetailsRepo.save(user);
-//        };
-//    }
 }
